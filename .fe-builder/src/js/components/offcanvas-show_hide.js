ae6e2b94affb02a12e85ca224be73e1db7527236
1Egor1Kuz1 <90701801+1Egor1Kuz1@users.noreply.github.com>
2021-11-24 18:37:09 +0500

    main page

--- .fe-builder/src/js/components/offcanvas-show_hide.js
+++ .fe-builder/src/js/components/offcanvas-show_hide.js
@@ -9,13 +9,15 @@ function initMenu(menu) {
     const subMenus = menu.querySelectorAll('li ul');
     for (const subMenu of subMenus) {
         const parentLi = subMenu.closest('li');
-        parentLi.addEventListener('click', (e) => {
+        const liTitle =parentLi.querySelector("a")
+        liTitle.addEventListener('click', (e) => {
             e.preventDefault();
             parentLi.classList.toggle('open');
             subMenu.classList.toggle('open');
         });
     }
 }
+
 
 
 
