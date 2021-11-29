725a234ece9b26f1c9f9739128471e154b100039
1Egor1Kuz1 <90701801+1Egor1Kuz1@users.noreply.github.com>
2021-11-29 18:25:20 +0500

    Merge branch 'master' of https://github.com/1Egor1Kuz1/school-16

--- .fe-builder/src/js/components/my-swiper.js
+++ .fe-builder/src/js/components/my-swiper.js
@@ -2,19 +2,21 @@ import Swiper from 'swiper/bundle';
 // import 'swiper/swiper.min.css';
 
 const swiper = new Swiper(".mySwiper", {
-    effect: "fade",
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+  effect: "fade",
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
     },
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-      },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-  
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
