f0607d1edeff9248de2e90750f6b4cc015452449
1Egor1Kuz1 <90701801+1Egor1Kuz1@users.noreply.github.com>
2021-12-11 19:28:16 +0500

    main page

--- .fe-builder/src/js/components/my-swiper.js
+++ .fe-builder/src/js/components/my-swiper.js
@@ -8,12 +8,11 @@ const swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  
   pagination: {
+    
     el: ".swiper-pagination",
     clickable: true,
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + "</span>";
-    },
   },
   autoplay: {
     delay: 5000,
